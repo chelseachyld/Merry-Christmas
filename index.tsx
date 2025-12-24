216afba0498b11a67557c78a03a7e4cffeb5e842
chelseachyld <3262565028@qq.com>
2025-12-24 14:48:50 +0800

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,11 +1,9 @@
-// index.tsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './index.css';
 
-// 获取 root 容器
 const rootElement = document.getElementById('root');
-
 if (rootElement) {
   const root = ReactDOM.createRoot(rootElement);
   root.render(
